fd50911d539d7eb064055c07a8d42118e3c9f7bf
William Whitaker <william@williamwhitaker.dev>
2023-02-01 15:47:39 -0500

    removes npmrc from repo

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <div class="m-container">
+      <h1>mote</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: black;
+  color: white;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,28 @@
-<script setup></script>
+<script setup>
+import SiteHeader from '../components/SiteHeader.vue';
+</script>
 
 <template>
-  <main>
-    <h1>mote</h1>
-    <ul>
-      <li>hi</li>
-    </ul>
-  </main>
+  <SiteHeader />
+  <div class="m-container">
+    <main>
+      <div class="m-grid m-grid--side-by-side">
+        <article>
+          <div class="m-grid m-grid--side-by-side">
+            <header>
+              <h2>mote is...</h2>
+            </header>
+            <section class="show-me">
+              <ul>
+                <li>typography</li>
+                <li>colors</li>
+                <li>components</li>
+                <li>grid</li>
+              </ul>
+            </section>
+          </div>
+        </article>
+      </div>
+    </main>
+  </div>
 </template>
