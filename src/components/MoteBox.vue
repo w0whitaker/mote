6f3377f57bfc3e806947e2b1ab4ca047a0aa2e97
William Whitaker <william@williamwhitaker.dev>
2023-02-05 13:55:14 -0500

    creates stacks and boxes

--- src/components/MoteBox.vue
+++ src/components/MoteBox.vue
@@ -8,16 +8,37 @@ export default {
 };
 </script>
 <template>
-	<div class="mote-box" :style="{ color }">
+	<div class="mote-box">
 		<slot name="content" />
 	</div>
 </template>
 <style>
 .mote-box {
-	border: 1px solid hotpink;
+	--color-light: hsl(0, 0%, 80%);
+	--color-dark: hsl(0, 0%, 20%);
+	color: var(--color-dark);
+	background-color: var(--color-light);
+	outline: 0.125rem solid transparent;
+	outline-offset: -0.125rem;
+}
+
+.mote-box.invert {
+	filter: invert(100%);
 }
 
 .small-pad {
 	padding: var(--space-s);
+}
+
+.medium-pad {
+	padding: var(--space-m);
+}
+
+.large-pad {
+	padding: var(--space-l);
+}
+
+.jumbo-pad {
+	padding: var(--space-2xl);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,101 @@
 <script setup>
 import SiteHeader from '../components/SiteHeader.vue';
+import MoteStack from '../components/MoteStack.vue';
 import MoteBox from '../components/MoteBox.vue';
-import Titans from '../components/stories/titans.js';
+import Paragraph from '../content/paragraph.js';
 </script>
 
 <template>
 	<SiteHeader />
 	<div class="m-container">
 		<main>
-			<MoteBox class="small-pad constrained">
-				<template v-slot:content>
-					<div v-html="Titans.template" class="show-me small-pad"></div>
-				</template>
-			</MoteBox>
+			<article>
+				<MoteStack>
+					<template v-slot:elements>
+						<h2>Boxes</h2>
+						<MoteStack>
+							<template v-slot:elements>
+								<!-- small padding -->
+								<section>
+									<MoteBox>
+										<template v-slot:content>
+											<MoteBox class="invert small-pad">
+												<template v-slot:content>
+													<header>
+														<h3>small</h3>
+													</header>
+												</template>
+											</MoteBox>
+											<MoteBox class="small-pad">
+												<template v-slot:content>
+													<p v-html="Paragraph.template"></p>
+												</template>
+											</MoteBox>
+										</template>
+									</MoteBox>
+								</section>
+								<!-- medium padding -->
+								<section>
+									<MoteBox>
+										<template v-slot:content>
+											<MoteBox class="invert medium-pad">
+												<template v-slot:content>
+													<header>
+														<h3>medium</h3>
+													</header>
+												</template>
+											</MoteBox>
+											<MoteBox class="small-pad">
+												<template v-slot:content>
+													<p v-html="Paragraph.template"></p>
+												</template>
+											</MoteBox>
+										</template>
+									</MoteBox>
+								</section>
+								<!-- large padding -->
+								<section>
+									<MoteBox>
+										<template v-slot:content>
+											<MoteBox class="invert large-pad">
+												<template v-slot:content>
+													<header>
+														<h3>large</h3>
+													</header>
+												</template>
+											</MoteBox>
+											<MoteBox class="large-pad">
+												<template v-slot:content>
+													<p v-html="Paragraph.template"></p>
+												</template>
+											</MoteBox>
+										</template>
+									</MoteBox>
+								</section>
+								<!-- jumbo padding -->
+								<section>
+									<MoteBox>
+										<template v-slot:content>
+											<MoteBox class="invert jumbo-pad">
+												<template v-slot:content>
+													<header>
+														<h3>jumbo</h3>
+													</header>
+												</template>
+											</MoteBox>
+											<MoteBox class="jumbo-pad">
+												<template v-slot:content>
+													<p v-html="Paragraph.template"></p>
+												</template>
+											</MoteBox>
+										</template>
+									</MoteBox>
+								</section>
+							</template>
+						</MoteStack>
+					</template>
+				</MoteStack>
+			</article>
 		</main>
 	</div>
 </template>
