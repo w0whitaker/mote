2009cda9e3388b5b64c9bece46b82eb79e4e64a7
William Whitaker <william@williamwhitaker.dev>
2023-02-05 19:48:14 -0500

    removes logo

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,28 +1,16 @@
 <script></script>
 
 <template>
-	<header>
-		<div class="m-container">
-			<div class="flex-container">
-				<h1>mote</h1>
-				<img src="./icons/mote.svg" alt="the logo" />
-			</div>
-		</div>
-	</header>
+  <header>
+    <div class="m-container">
+      <h1>mote</h1>
+    </div>
+  </header>
 </template>
 
 <style scoped>
 header {
-	background-color: black;
-	color: white;
-}
-
-img {
-	max-height: var(--step-5);
-}
-
-.flex-container {
-	display: flex;
-	align-items: center;
+  background-color: black;
+  color: white;
 }
 </style>
