0335eab38df83022eddebac74a73bd2f6395614f
William Whitaker <william@williamwhitaker.dev>
2023-02-05 19:43:25 -0500

    sets up boxes article

--- src/components/MoteBox.vue
+++ src/components/MoteBox.vue
@@ -1,44 +1,36 @@
-<script>
-export default {
-	data() {
-		return {
-			color: 'black',
-		};
-	},
-};
-</script>
+<script></script>
 <template>
-	<div class="mote-box">
-		<slot name="content" />
-	</div>
+  <div class="mote-box">
+    <slot name="content" />
+  </div>
 </template>
 <style>
 .mote-box {
-	--color-light: hsl(0, 0%, 80%);
-	--color-dark: hsl(0, 0%, 20%);
-	color: var(--color-dark);
-	background-color: var(--color-light);
-	outline: 0.125rem solid transparent;
-	outline-offset: -0.125rem;
+  --color-light: hsl(0, 0%, 80%);
+  --color-dark: hsl(0, 0%, 20%);
+  color: var(--color-dark);
+  background-color: var(--color-light);
+  outline: 0.125rem solid transparent;
+  outline-offset: -0.125rem;
 }
 
 .mote-box.invert {
-	filter: invert(100%);
+  filter: invert(100%);
 }
 
 .small-pad {
-	padding: var(--space-s);
+  padding: var(--space-s);
 }
 
 .medium-pad {
-	padding: var(--space-m);
+  padding: var(--space-m);
 }
 
 .large-pad {
-	padding: var(--space-l);
+  padding: var(--space-l);
 }
 
 .jumbo-pad {
-	padding: var(--space-2xl);
+  padding: var(--space-2xl);
 }
 </style>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,26 @@
+<script></script>
+
+<template>
+  <footer>
+    <div class="m-container">
+      <div class="flex-container">
+        <p>mote</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: black;
+  color: white;
+}
+
+p {
+  font-size: var(--step-3);
+}
+s.flex-container {
+  display: flex;
+  align-items: center;
+}
+</style>
