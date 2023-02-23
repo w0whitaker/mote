ab2c66feb6403c21f0b82cb0a84b2cb4a0d94d58
William Whitaker <william@williamwhitaker.dev>
2023-02-23 14:40:40 -0500

    Squashed commit of the following:
    commit fe54d43d435289afc6595efc877a47d2ffb0ccbb
    Author: William Whitaker <william@williamwhitaker.dev>
    Date:   Thu Feb 23 14:37:50 2023 -0500
    
        finishes up buttons for now
    
    commit 65c230ffc20c8fbf359bd5fcc2ccbe0f53d051b0
    Author: William Whitaker <william@williamwhitaker.dev>
    Date:   Wed Feb 22 20:27:20 2023 -0500
    
        provides button options

---
+++ src/components/MoteButton.stories.js
@@ -0,0 +1,60 @@
+import MoteButton from "./MoteButton.vue";
+
+export default {
+  title: "MoteButton",
+  component: MoteButton,
+  decorators: [
+    () => ({ template: '<div style="margin: 3em;"><story /></div>' }),
+  ],
+  parameters: {
+    controls: { sort: "requiredFirst" },
+  },
+  argTypes: {
+    use: {
+      table: {
+        disable: true,
+      },
+    },
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "select" },
+    },
+    variant: {
+      options: ["solid", "outline", "disabled", "subtle"],
+      control: { type: "select" },
+    },
+    label: {
+      control: { type: "text" },
+    },
+  },
+};
+
+const Template = (args) => ({
+  components: { MoteButton },
+  setup() {
+    return { args };
+  },
+  template: '<MoteButton v-bind="args"',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  use: "primary",
+  size: "medium",
+};
+export const Secondary = Template.bind({});
+Secondary.args = {
+  use: "secondary",
+};
+export const Success = Template.bind({});
+Success.args = {
+  use: "success",
+};
+export const Danger = Template.bind({});
+Danger.args = {
+  use: "danger",
+};
+export const Warning = Template.bind({});
+Warning.args = {
+  use: "warning",
+};
--- src/components/MoteButton.vue
+++ src/components/MoteButton.vue
@@ -1,90 +1,50 @@
+<template>
+  <button :class="classes">
+    {{ label }}
+  </button>
+</template>
+
 <script setup>
 import { computed, ref } from 'vue';
+import './mote-button.css';
 const props = defineProps({
+  label: {
+    type: String,
+    default: 'button',
+    required: true,
+  },
+  size: {
+    type: String,
+    default: 'medium',
+    validator(value) {
+      return ['small', 'medium', 'large'].includes(value);
+    },
+  },
   variant: {
     type: String,
     default: 'standard',
     validator(value) {
-      return ['standard', 'outlined', 'disabled', 'clear'].includes(value);
+      return ['solid', 'outline', 'disabled', 'subtle'].includes(value);
+    },
+  },
+  use: {
+    type: String,
+    default: 'primary',
+    validator(value) {
+      return ['primary', 'secondary', 'success', 'danger', 'warning'].includes(
+        value
+      );
     },
   },
 });
+
 const variant = ref(props.variant);
-class ButtonStyle {
-  constructor({ backgroundColor, color, outline, outlineOffset }) {
-    this.backgroundColor = backgroundColor;
-    this.color = color;
-    this.outline = outline;
-    this.outlineOffset = outlineOffset;
-  }
-}
-const styleObject = computed(() => {
-  switch (variant.value) {
-    case 'standard':
-      return new ButtonStyle({
-        backgroundColor: 'var(--secondary)',
-        color: 'var(--dark)',
-        outline: 'none',
-        outlineOffset: '0',
-      });
-    case 'outlined':
-      return new ButtonStyle({
-        backgroundColor: 'var(--light)',
-        color: 'var(--dark)',
-        outline: '0.125rem solid var(--primary)',
-        outlineOffset: '-0.125rem',
-      });
-    case 'disabled':
-      return new ButtonStyle({
-        backgroundColor: 'var(--dark)',
-        color: 'var(--light)',
-        outline: 'none',
-        outlineOffset: '0',
-      });
-    case 'clear':
-      return new ButtonStyle({
-        backgroundColor: 'transparent',
-        color: 'var(--dark)',
-        outline: 'initial',
-        outlineOffset: '0',
-      });
-    default:
-      return new ButtonStyle({
-        backgroundColor: 'hotpink',
-        color: 'yellowgreen',
-        outline: '0.125rem solid pink',
-        outlineOffset: '-0.125rem',
-      });
-  }
-});
+const use = ref(props.use);
+const size = ref(props.size);
+const classes = computed(() => ({
+  '[ mote-button ]': true,
+  [`[ mote-button--${use.value || 'primary'} ]`]: true,
+  [`[ mote-button--${size.value || 'medium'} ]`]: true,
+  [`[ mote-button--${variant.value || 'solid'} ]`]: true,
+}));
 </script>
-<template>
-  <button class="mote-button" :style="styleObject">
-    {{ variant }}
-  </button>
-</template>
-<style scoped>
-button {
-  padding: var(--space-2xs) var(--space-xl);
-  font-size: var(--step-1);
-  font-variant-caps: all-small-caps;
-  border: none;
-  border-radius: 5px;
-}
-.mote-button--default {
-  background-color: white;
-  color: black;
-}
-
-.mote-button--outlined {
-  background-color: white;
-  color: black;
-  outline: 0.125rem solid black;
-  outline-offset: -0.125rem;
-}
-
-.mote-button--clear {
-  background-color: transparent;
-  color: black;
-}
-</style>
