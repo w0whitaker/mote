38ac3f6f24f2b18457b5f0fe389e733bde6298a1
William Whitaker <william@williamwhitaker.dev>
2023-02-27 19:14:57 -0500

    fixes some story errors

--- src/components/MoteButton.stories.js
+++ src/components/MoteButton.stories.js
@@ -31,27 +31,41 @@ const Template = (args) => ({
   setup() {
     return { args };
   },
-  template: '<MoteButton v-bind="args"',
+  template: '<MoteButton v-bind="args" />',
 });
 
 export const Primary = Template.bind({});
 Primary.args = {
   use: 'primary',
   size: 'medium',
+  label: 'button',
+  variant: 'solid',
 };
 export const Secondary = Template.bind({});
 Secondary.args = {
   use: 'secondary',
+  size: 'medium',
+  label: 'button',
+  variant: 'solid',
 };
 export const Success = Template.bind({});
 Success.args = {
   use: 'success',
+  size: 'medium',
+  label: 'button',
+  variant: 'solid',
 };
 export const Danger = Template.bind({});
 Danger.args = {
   use: 'danger',
+  size: 'medium',
+  label: 'button',
+  variant: 'solid',
 };
 export const Warning = Template.bind({});
 Warning.args = {
   use: 'warning',
+  size: 'medium',
+  label: 'button',
+  variant: 'solid',
 };
